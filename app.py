6d54e81a9b55d0513c23c8b9719d3f9379ee2c7d
msaoudallah <msaoudallah@gmail.com>
2022-01-24 15:25:22 +0200

    update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 
 
-@app.route('/{}'.format(TOKEN) , method=['POST'])
+@app.route('/{}'.format(TOKEN) , methods=['POST'])
 def respond():
     update = telegram.Update.de_json(request.get_json(force=True),bot)
     

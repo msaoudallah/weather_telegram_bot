f3261a68178131596922797d6632c74f2618b567
msaoudallah <msaoudallah@gmail.com>
2022-01-24 15:11:35 +0200

    update app file

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def respond():
     return 'ok'
 
 
-@app.route('/setwebhook', method = ['POST'])
+@app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
     s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOk=TOKEN))
     if s:

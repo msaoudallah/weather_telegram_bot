3df088af5d4075de404bb76a730f9b24cdf42fba
msaoudallah <msaoudallah@gmail.com>
2022-01-25 21:36:38 +0200

    update credential files

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
-bot_token = '5259969493:AAE-N3QWplKLz1nmAyi7KXalA8lrbVSQxV8'
+bot_token = 'TOKEN_HERE'
 bot_user_name = 'msaid_1stbot'
-URL ='https://msaid-telebot1.herokuapp.com/'
+URL = 'https://msaid-telebot1.herokuapp.com/'
